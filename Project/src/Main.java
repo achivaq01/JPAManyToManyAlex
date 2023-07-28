0ff01b4b7fb8707481f5da466c68f05f662aad95
optimisme <optimisme@gmail.com>
2023-07-28 18:44:45 +0200

    Use 'data' folder

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,6 +21,16 @@ public class Main {
 
    public static void main(String[] args) {
       
+      String basePath = System.getProperty("user.dir") + "/data/";
+
+      // Crear la carpeta 'data' si no existeix
+      File dir = new File(basePath);
+      if (!dir.exists()){
+         if(!dir.mkdirs()) {
+               System.out.println("Error en la creació de la carpeta 'data'");
+         }
+      }
+
       Manager.createSessionFactory();
 
       Employee refEmp1 = Manager.addEmployee("Employee 1", "Aes", 1000);
